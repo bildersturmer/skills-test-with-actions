304afbd294b0b173892eee958462d504df848922
bilder sturmer <222788240+bildersturmer@users.noreply.github.com>
2025-08-28 12:40:34 +0000

    Two new tests added def test_area_of_circle_negative_radius(): def test_get_nth_fibonacci_negative_input(): REQUIRED: import pytest

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -1,6 +1,7 @@
 # System Modules
 import sys
 import os
+import pytest
 
 # Installed Modules
 # None
@@ -68,3 +69,13 @@ def test_get_nth_fibonacci_ten():
  
     # Assert
     assert result == 55
+
+
+def test_area_of_circle_negative_radius():
+    with pytest.raises(ValueError, match="Radius cannot be negative"):
+        area_of_circle(-5)
+
+
+def test_get_nth_fibonacci_negative_input():
+    with pytest.raises(ValueError, match="n cannot be negative"):
+        get_nth_fibonacci(-3)
